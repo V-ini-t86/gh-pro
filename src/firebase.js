0a5f0769497717ec32cf0c4f7ea80de1ce052065
Vinit Joshi <vinitjoshi.2345@gmail.com>
2021-10-11 23:24:48 +0530

    Adding github workflow

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import * as firebase from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCK2cxU-o4UT00cI9pyAKcITN93b27Jsh0",
@@ -11,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: "769028615372",
   appId: "1:769028615372:web:bc8cb51c68e5ae52b6e669",
   measurementId: "G-RWRYF48MEG",
-}; 
+};
 
 firebase.initializeApp(firebaseConfig);
 const storage = getStorage();
@@ -20,4 +20,5 @@ const auth = getAuth();
 const provider = new GoogleAuthProvider();
 const db = getFirestore();
 export default firebase;
-export { auth, provider, db,storage };
+export { auth, provider, db, storage };
+// 1//0gDvyDj-HshkRCgYIARAAGBASNwF-L9IrxzkcbyXXKXaeU4fJab-bYEJqA20FqI1MFrpi18Y6DuNHQbeu-l9ksuSp4mHIYL8z2bk
